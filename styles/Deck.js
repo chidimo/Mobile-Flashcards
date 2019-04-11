2548875fcdd396811a781adb537a32d56403a18b
Chidi Orji <orjichidi95@gmail.com>
2019-04-11 15:57:39 +0100

    css: Style new card container

--- styles/Deck.js
+++ styles/Deck.js
@@ -11,4 +11,17 @@ export default deckStyles = StyleSheet.create({
         color: 'midnightblue',
         fontWeight: '900',
     },
+    itemContainer: {
+        padding: 8,
+        alignSelf: 'stretch',
+        backgroundColor: 'lightblue',
+        borderRadius: 2,
+        marginBottom: 1,
+        borderWidth: StyleSheet.hairlineWidth,
+    },
+    text: {
+        color: 'blue',
+        textAlign: 'center',
+        fontWeight: '700',
+    }
 })
