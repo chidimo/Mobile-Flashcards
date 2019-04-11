1401d2c00f58093094f657a5da2d8cf9b45f6c77
Chidi Orji <orjichidi95@gmail.com>
2019-04-11 12:14:19 +0100

    feat: Add icons to nav tabs

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,5 +1,11 @@
-// import React, { Component } from 'react';
-import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import React from 'react';
+import { 
+    createBottomTabNavigator,
+    createStackNavigator,
+    createAppContainer 
+} from 'react-navigation';
+import { Platform } from 'react-native';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
 import Decks from './Decks'
 import NewDeck from './NewDeck'
@@ -32,15 +38,34 @@ const NewDeckStack = createStackNavigator(
 
 const tabs = createBottomTabNavigator(
     {
-        Decks: {
-            screen: DeckStack
+        Add: {
+            screen: NewDeckStack,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+            }
         },
 
-        Add: {
-            screen: NewDeckStack
+        Decks: {
+            screen: DeckStack,
+            navigationOptions: {
+                title: 'Home',
+                tabBarIcon: ({ tintColor }) => <Ionicons name={Platform.OS === 'ios' ? 'ios-home' : 'md-home'} size={30} color={tintColor}/>
+            }
+        },
+    },
+
+    {
+        tabBarOptions: {
+            activeTintColor: 'black',
+            labelStyle: {
+                fontSize: 12,
+            },
+            style: {
+                color: 'black',
+                backgroundColor: '#2980b6',
+            },
         }
     }
 )
-
 
 export default Navigation = createAppContainer(tabs)
