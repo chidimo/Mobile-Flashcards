52686b45701afcc0584fc42dc814b122f79934c5
Chidi Orji <orjichidi95@gmail.com>
2019-04-11 15:29:29 +0100

    css: Style deck component

--- components/Deck.js
+++ components/Deck.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import deckStyles from '../styles/Deck';
+
 class Deck extends Component {
     render() {
         const { navigation } = this.props
 
         return (
-            <View>
-                <Text>
+            <View style={deckStyles.container}>
+                <Text style={deckStyles.headingText}>
                     {navigation.state.params.item}
                 </Text>
             </View>
