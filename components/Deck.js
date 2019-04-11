6da59e4a5c7dffa71af9e1157f59651537b1c8a0
Chidi Orji <orjichidi95@gmail.com>
2019-04-11 15:55:55 +0100

    feat: Activate new card route

--- components/Deck.js
+++ components/Deck.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import deckStyles from '../styles/Deck';
 
 class Deck extends Component {
     render() {
         const { navigation } = this.props
+        const item = navigation.state.params.item
 
         return (
             <View style={deckStyles.container}>
                 <Text style={deckStyles.headingText}>
-                    {navigation.state.params.item}
+                    {item.toUpperCase()}
                 </Text>
+
+                <TouchableOpacity
+                    style={deckStyles.itemContainer}
+                    onPress={() => this.props.navigation.navigate(
+                        'NewCard', { item }
+                    )}
+                >
+                    <Text style={deckStyles.text}>Add Card</Text>
+                </TouchableOpacity>
             </View>
         )
     }
--- components/NewCard.js
+++ components/NewCard.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class NewCard extends Component {
+    
     render() {
+        const { navigation } = this.props
+        const deck = navigation.state.params.item
+
         return (
             <View>
                 <Text>
-                    Add new card
+                    {`Add card to ${deck}`} 
                 </Text>
             </View>
         )
