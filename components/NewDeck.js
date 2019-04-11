8dea04b8f4c839dfe3e1c99c41b2c467fbd44c72
Chidi Orji <orjichidi95@gmail.com>
2019-04-11 12:15:55 +0100

    feat: Finish up new deck component

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import { View, Text } from 'react-native';
+import { View, Text,   } from 'react-native';
+import SharedTextInput from './Shared.TextInput'
+import SharedButton from './Shared.Button'
+
+import { add_new_deck_handler } from '../actions/decks'
 
 class NewDeck extends Component {
+
+    state = { name: '' }
+
+    _save_deck = () => {
+        const { name } = this.state
+        this.props.add_deck(name)
+    }
+
     render() {
         return (
             <View>
                 <Text>
-                    Add a new deck
+                    Add new deck
                 </Text>
+
+                <SharedTextInput
+                    placeholder={'Enter deck name'}
+                    onChangeText={name => this.setState({name})}
+                />
+
+                <SharedButton
+                    label={'Save deck'}
+                    onPress={this._save_deck}
+                />
             </View>
         )
     }
 }
 
-export default NewDeck;
+const mapDispatchToProps = dispatch => {
+    const add_deck = name => {
+        dispatch(add_new_deck_handler({name}))
+    }
+
+    return {
+        add_deck
+    }
+}
+
+export default connect(null, mapDispatchToProps)(NewDeck);
