a4947eb54bbaf25a48a1a63e5d9a38ff2467b302
Chidi Orji <orjichidi95@gmail.com>
2019-04-11 15:00:37 +0100

    feat: Load stored decks on mount

--- components/Decks.js
+++ components/Decks.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage, FlatList, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+
+import { get_decks } from '../actions/decks'
+import decksStyles from '../styles/Decks';
 
 class Decks extends Component {
+    state = { ready: false }
+
+    componentDidMount() {
+        const { dispatch } = this.props
+        AsyncStorage.getAllKeys()
+        .then(decks => {
+            dispatch(get_decks(decks))
+            this.setState({ready: true})
+        })
+    }
+
+    render_deck = (item) => (
+        <TouchableOpacity
+            style={decksStyles.itemContainer}
+            onPress={() => console.log(`${item} pressed`)}
+        >
+            <Text style={decksStyles.text}>{item.toUpperCase()}</Text>
+        </TouchableOpacity>
+    )
+
     render() {
+        const { decks } = this.props
         return(
-            <View>
-                <Text>Decks</Text>
+            <View style={decksStyles.container}>
+                <Text style={decksStyles.headingText}>Decks</Text>
+
+                <FlatList
+                    data={decks}
+                    renderItem={ ({ item }) => (this.render_deck(item))}
+                    keyExtractor={(item, index) => index.toString()}
+                />
             </View>
         )
     }
 }
 
-export default Decks
+const mapStateToProps = ({ decks }) => {
+    return { decks }
+}
+
+export default connect(mapStateToProps)(Decks)
