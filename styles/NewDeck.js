ed0b6df004f6aa5b658528532c3ef9c444303053
Chidi Orji <orjichidi95@gmail.com>
2019-04-11 15:25:59 +0100

    css: Add styles for new deck

---
+++ styles/NewDeck.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+export default newDeckStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+    },
+    headingText: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: 'midnightblue'
+    },
+})
