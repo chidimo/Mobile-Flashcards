eb9fc6a6d77ab1c32d1b815918640d647e73f0d7
Chidi Orji <orjichidi95@gmail.com>
2019-04-12 20:24:54 +0100

    feat: Add a card to AsyncStorage

--- actions/cards.js
+++ actions/cards.js
@@ -2,21 +2,33 @@ import { AsyncStorage } from 'react-native';
 
 import { ADD_CARD } from './types'
 
-export const add_card = ({ deck, question, answer }) => {
-    const key = `${deck}:cards`
+export const add_card = ({ deck, result }) => {
     return {
         type: ADD_CARD,
-        key,
-        question,
-        answer,
+        deck,
+        result,
     }
 }
 
 export const add_card_handler = (info_object) => {
     const { deck, question, answer } = info_object
-    const key = `${deck}:cards`
+    const card = { question, answer }
+
+    // each key in AsyncStorage is the name of a deck
+    // the value is a string which is formed by concatenating
+    // stringified question and answer objects of the form
+    // { question: 'some question', answer: 'some answer' }
+    // the concatenating string is ":"
+
     return dispatch => {
-        AsyncStorage.setItem(key, JSON.stringify({ question, answer }))
-        .then(dispatch(add_card(info_object)))
+        AsyncStorage.getItem(deck)
+        .then(existing_card => {
+            const updated_card = `${existing_card !== null ? existing_card : ''}${existing_card !== null ? ':' : ''}${JSON.stringify(card)}`
+            AsyncStorage.setItem(deck, updated_card)
+            .then(result => {
+                console.log('result; ', result)
+                dispatch(add_card({ deck, result }))
+            })
+        })
     }
 }
