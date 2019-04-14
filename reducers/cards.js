60ec65f37b2892d2d10e8eecc495b469f443052d
Chidi Orji <orjichidi95@gmail.com>
2019-04-15 00:27:42 +0100

    refactor: Refactor local state update

--- reducers/cards.js
+++ reducers/cards.js
@@ -1,5 +1,6 @@
 import { ADD_CARD, GET_CARDS } from '../actions/types';
 
+
 export const cards = (state=[], action) => {
     switch (action.type) {
 
@@ -7,7 +8,8 @@ export const cards = (state=[], action) => {
             return action.cards
 
         case ADD_CARD:
-            return action.updated_cards
+            let q = { question: action.question, answer: action.answer}
+            return state.concat([{ deckname: action.deckname, quiz: q }])
 
         default:
             return state
