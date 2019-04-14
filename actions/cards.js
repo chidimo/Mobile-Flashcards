80effc6aeeeaf8b36fa7bc5581bab8a76ae7562f
Chidi Orji <orjichidi95@gmail.com>
2019-04-15 00:25:21 +0100

    refactor: Pass question and answer directly to reducer

--- actions/cards.js
+++ actions/cards.js
@@ -2,28 +2,27 @@ import { AsyncStorage } from 'react-native';
 
 import { ADD_CARD, GET_CARDS } from './types'
 
-export const add_card = ({ deckname, quiz }) => {
+export const add_card = ({ deckname, question, answer }) => {
     return {
         type: ADD_CARD,
         deckname,
-        quiz,
+        question,
+        answer,
     }
 }
 
 export const add_card_handler = (info_object) => {
     const { deckname, question, answer } = info_object
-    
     return dispatch => {
         AsyncStorage.getItem('store')
         .then(JSON.parse)
         .then(store => {
-            store[deckname].questions.push({ question, answer })
             console.log('\n\nstore', JSON.stringify(store), '\n\n')
+            store[deckname].questions.push({ question, answer })
 
             AsyncStorage.setItem('store', JSON.stringify(store))
             .then(() => {
-                const quiz = { question, answer }
-                dispatch(add_card({ deckname, quiz }))
+                dispatch(add_card({ deckname, question, answer }))
             })
         })
     }
