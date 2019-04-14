0791fd9004f5b4c44f151ad01858d821ff75ec59
Chidi Orji <orjichidi95@gmail.com>
2019-04-14 23:07:46 +0100

    refactor: Refactor for new store shape

--- actions/cards.js
+++ actions/cards.js
@@ -2,29 +2,28 @@ import { AsyncStorage } from 'react-native';
 
 import { ADD_CARD, GET_CARDS } from './types'
 
-export const add_card = ({ updated_cards }) => {
+export const add_card = ({ deckname, quiz }) => {
     return {
         type: ADD_CARD,
-        updated_cards,
+        deckname,
+        quiz,
     }
 }
 
 export const add_card_handler = (info_object) => {
-    const { deck, question, answer } = info_object
+    const { deckname, question, answer } = info_object
     
     return dispatch => {
-        AsyncStorage.getItem('cards')
+        AsyncStorage.getItem('store')
         .then(JSON.parse)
-        .then(cards => {
-            
-            let updated_cards = cards.concat(
-                { deckname: deck, quiz: { question, answer } }
-            )
+        .then(store => {
+            store[deckname].questions.push({ question, answer })
+            console.log('\n\nstore', JSON.stringify(store), '\n\n')
 
-            AsyncStorage.setItem('cards', JSON.stringify(updated_cards))
-            .then(JSON.parse)
-            .then(data => {
-                dispatch(add_card({ updated_cards }))
+            AsyncStorage.setItem('store', JSON.stringify(store))
+            .then(() => {
+                const quiz = { question, answer }
+                dispatch(add_card({ deckname, quiz }))
             })
         })
     }
